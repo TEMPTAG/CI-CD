786fef481d4e8204483f39e26a6041e78b053379
Ian Ferguson <iansterlingferguson@gmail.com>
2024-11-12 11:10:25 -0700

    added node engines to package.jsons

--- server/dist/seeds/seed.js
+++ server/dist/seeds/seed.js
@@ -1,10 +1,16 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
-db.once('open', async () => {
-    await cleanDB('Question', 'questions');
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+// Define __dirname for ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pythonQuestions = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./pythonQuestions.json"), "utf-8"));
+db.once("open", async () => {
+    await cleanDB("Question", "questions");
     await Question.insertMany(pythonQuestions);
-    console.log('Questions seeded!');
+    console.log("Questions seeded!");
     process.exit(0);
 });
