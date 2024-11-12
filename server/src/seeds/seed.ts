e481459db39e7ca3493c67dbecd153766a8f0ec5
Ian Ferguson <iansterlingferguson@gmail.com>
2024-11-12 10:26:00 -0700

    seed.ts fixes.

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -3,6 +3,11 @@ import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname for ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pythonQuestions = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "./pythonQuestions.json"), "utf-8")
